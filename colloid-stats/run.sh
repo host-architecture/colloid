6715ef796fca10f49cc94bebf2761d2c6c3b300a
Midhul <midhul.v@gmail.com>
2023-09-29 23:54:22 +0000

    throughput logging in gups

--- colloid-stats/run.sh
+++ colloid-stats/run.sh
@@ -25,9 +25,9 @@ for i in 8 4 2 1; do
         eatmem=$(($local_capacity_mib-$lmem))
         insmod /home/midhul/colloid/memeater/memeater.ko sizeMiB=$eatmem;
         cat /sys/devices/system/node/node3/meminfo | grep "MemFree";
-        /home/midhul/mio-colloid/gups/gups-rw $i &
+        /home/midhul/colloid/gups/gups-rw $i &
         pid_gups=$!;
-        taskset -c 0 /home/midhul/mio-colloid/colloid-stats/record_stats > /home/midhul/membw-eval/$config_rw-cores$i-lmem$lmem.stats.txt 2>&1 &
+        taskset -c 0 /home/midhul/colloid/colloid-stats/record_stats > /home/midhul/membw-eval/$config_rw-cores$i-lmem$lmem.stats.txt 2>&1 &
         pid_stats=$!;
         sleep 1000;
         killall record_stats;
@@ -49,9 +49,9 @@ for i in 8 4 2 1; do
         eatmem=$(($local_capacity_mib-$lmem))
         insmod /home/midhul/colloid/memeater/memeater.ko sizeMiB=$eatmem;
         cat /sys/devices/system/node/node3/meminfo | grep "MemFree";
-        /home/midhul/mio-colloid/gups/gups-r $i &
+        /home/midhul/colloid/gups/gups-r $i &
         pid_gups=$!;
-        taskset -c 0 /home/midhul/mio-colloid/colloid-stats/record_stats > /home/midhul/membw-eval/$config_r-cores$i-lmem$lmem.stats.txt 2>&1 &
+        taskset -c 0 /home/midhul/colloid/colloid-stats/record_stats > /home/midhul/membw-eval/$config_r-cores$i-lmem$lmem.stats.txt 2>&1 &
         pid_stats=$!;
         sleep 1000;
         killall record_stats;
--- gups/gups.c
+++ gups/gups.c
@@ -12,6 +12,10 @@
 #include <fcntl.h>
 #include <sched.h>
 #include <pthread.h>
+#include <stdatomic.h>
+
+#define LOG_INTERVAL_MS 1000
+#define MAX_THREADS 8
 
 // #define WSS 103079215104ULL
 #define WSS 77309411328ULL
@@ -19,10 +23,33 @@
 // #define WSS 2147483648ULL
 // #define HOTSS 1073741824ULL
 
+int TSC_ratio;
+uint64_t begin_ts;
+
+static inline __attribute__((always_inline)) unsigned long rdtsc()
+{
+   unsigned long a, d;
+
+   __asm__ volatile("rdtsc" : "=a" (a), "=d" (d));
+
+   return (a | (d << 32));
+}
+
+
+static inline __attribute__((always_inline)) unsigned long rdtscp()
+{
+   unsigned long a, d, c;
+
+   __asm__ volatile("rdtscp" : "=a" (a), "=d" (d), "=c" (c));
+
+   return (a | (d << 32));
+}
+
 typedef struct {
     int thread_id;
     size_t buf_size;
     size_t hot_size;
+    _Atomic uint64_t *count_ptr;
 } ThreadArgs;
 
 void *thread_function(void *arg) {
@@ -33,48 +60,71 @@ void *thread_function(void *arg) {
         printf("mmap failed\n");
         return NULL;
     }
+
+    uint64_t cur_ts=0, prev_ts=0;
+    cur_ts = rdtscp();
+    prev_ts = cur_ts;
     
     // printf("allocated %lu buf\n", args->buf_size);
     memset(a, 'm', args->buf_size);
     uint64_t x = 432437644 + args->thread_id;
-    uint64_t count = 0;
+    uint64_t count = 0, prev_count = 0;
     __m512i sum = _mm512_set_epi32(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
     __m512i val = _mm512_set_epi32(1995, 1995, 2002, 2002, 1995, 1995, 2002, 2002, 1995, 1995, 2002, 2002, 1995, 1995, 2002, 2002);
+    int i;
     while(count < 999999999999999ULL) {
-        char *start;
-        size_t slots;
-        x ^= x << 13;
-		x ^= x >> 7;
-		x ^= x << 17;
-        if(x%100 < 90) {
-            // access hot region
-            start = a + (args->buf_size - args->hot_size);
-            slots = args->hot_size / 4096;
-        } else {
-            start = a;
-            slots = (args->buf_size - args->hot_size)/4096;
-        }
+        for(i = 0; i < 1024; i++) {
+            char *start;
+            size_t slots;
+            x ^= x << 13;
+            x ^= x >> 7;
+            x ^= x << 17;
+            if(x%100 < 90) {
+                // access hot region
+                start = a + (args->buf_size - args->hot_size);
+                slots = args->hot_size / 4096;
+            } else {
+                start = a;
+                slots = (args->buf_size - args->hot_size)/4096;
+            }
 
-        // access random slot
-        x ^= x << 13;
-		x ^= x >> 7;
-		x ^= x << 17;
-        char *chunk = start + 4096*(x%slots);
-        // printf("a: %p\n", a);
-        // printf("chunk: %p\n", chunk);
-        int k;
-        for(k = 0; k < 64; k++) {
-            #if defined(WORKLOAD_READWRITE)
-			__m512i mm_a = _mm512_load_si512(&chunk[64*k]);
-		    _mm512_store_si512(&chunk[64*k], _mm512_add_epi32(mm_a, val));
-            #elif defined(WORKLOAD_READ)
-            __m512i mm_a = _mm512_load_si512(&chunk[64*k]);
-		    sum = _mm512_add_epi32(sum, mm_a);
-            #else
-            #error "Define WORKLOAD"
-            #endif
-		}
-        count++;
+            // access random slot
+            x ^= x << 13;
+            x ^= x >> 7;
+            x ^= x << 17;
+            char *chunk = start + 4096*(x%slots);
+            // printf("a: %p\n", a);
+            // printf("chunk: %p\n", chunk);
+            int k;
+            for(k = 0; k < 64; k++) {
+                #if defined(WORKLOAD_READWRITE)
+                __m512i mm_a = _mm512_load_si512(&chunk[64*k]);
+                _mm512_store_si512(&chunk[64*k], _mm512_add_epi32(mm_a, val));
+                #elif defined(WORKLOAD_READ)
+                __m512i mm_a = _mm512_load_si512(&chunk[64*k]);
+                sum = _mm512_add_epi32(sum, mm_a);
+                #else
+                #error "Define WORKLOAD"
+                #endif
+            }
+            count++;
+        }
+        atomic_store(args->count_ptr, count);
+        // cur_ts = rdtscp();
+        // printf("cur_ts: %lu, prev_ts: %lu\n", cur_ts, prev_ts);
+        // if(cur_ts - prev_ts >= LOG_INTERVAL_MS*TSC_ratio*100*1e3) {
+        //     printf("%lu %lu\n", cur_ts-begin_ts, count - prev_count);
+        //     prev_ts = cur_ts;
+        //     prev_count = count;
+        // }
+        // if(__builtin_expect(count % 1000 == 0, 0)) {
+        //     cur_ts = rdtscp();
+        //     if(__builtin_expect(cur_ts - prev_ts >= LOG_INTERVAL_MS*TSC_ratio*100*1e3, 0)) {
+        //         printf("%lu %lu\n", cur_ts-begin_ts, count - prev_count);
+        //         prev_ts = cur_ts;
+        //         prev_count = count;
+        //     }
+        // }
     }
 
 
@@ -119,24 +169,39 @@ void *thread_function(void *arg) {
 int main(int argc, char *argv[]) {
     int cores[8] = {3,7,11,15,19,23,27,31};
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <num_threads>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <num_threads> <stats-path>\n", argv[0]);
         return 1;
     }
 
     int num_threads = atoi(argv[1]);
-    if (num_threads <= 0) {
-        fprintf(stderr, "Number of threads must be a positive integer\n");
+    if (num_threads <= 0 || num_threads > MAX_THREADS) {
+        fprintf(stderr, "Number of threads invalid\n");
         return 1;
     }
 
-    pthread_t threads[num_threads];
-    ThreadArgs thread_args[num_threads];
+    // Get TSC frequency
+    int msr_fd;
+    ssize_t ret;
+    uint64_t msr_val;
+    msr_fd = open("/dev/cpu/0/msr", O_RDWR);
+    if(msr_fd == -1) {
+        fprintf(stderr, "An error occurred while opening msr file.\n");
+		return EXIT_FAILURE;
+    }
+    ret = pread(msr_fd, &msr_val, sizeof(msr_val), 0xCEL);
+    TSC_ratio = (msr_val & 0x000000000000ff00L) >> 8;
+
+    _Atomic uint64_t thread_counts[MAX_THREADS];
+    pthread_t threads[MAX_THREADS];
+    ThreadArgs thread_args[MAX_THREADS];
     cpu_set_t cpuset;
 
     for (int i = 0; i < num_threads; ++i) {
         thread_args[i].thread_id = i;
         thread_args[i].buf_size = ((WSS/4096ULL)/((size_t)num_threads))*4096ULL;
         thread_args[i].hot_size = ((HOTSS/4096ULL)/((size_t)num_threads))*4096ULL;
+        atomic_init(&thread_counts[i], 0);
+        thread_args[i].count_ptr = &thread_counts[i];
         
         CPU_ZERO(&cpuset);
         CPU_SET(cores[i], &cpuset);
@@ -150,6 +215,17 @@ int main(int argc, char *argv[]) {
             perror("pthread_setaffinity_np");
             return 1;
         }
+    }
+
+    uint64_t prev_op_count = 0;
+    while(1) {
+        sleep(1);
+        uint64_t cur_op_count = 0;
+        for(int i = 0; i < num_threads; i++) {
+            cur_op_count += atomic_load(&thread_counts[i]);
+        }
+        printf("%lu\n", cur_op_count - prev_op_count);
+        prev_op_count = cur_op_count;
     }
 
     for (int i = 0; i < num_threads; ++i) {
