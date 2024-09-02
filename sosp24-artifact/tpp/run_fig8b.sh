8995fafdfa3ce4baa427fbcc44f437db1fd2c824
Midhul <midhul.v@gmail.com>
2024-09-02 11:32:25 -0400

    Minor fixes

--- sosp24-artifact/tpp/run_fig8b.sh
+++ sosp24-artifact/tpp/run_fig8b.sh
@@ -8,7 +8,7 @@ source $scripts_path/config.sh
 
 for b in 0 5 10 15; do
     start_time=$(date +%s)
-    DRAMSIZE=20761804800 $scripts_path/linux.sh tpp-silo-ycsb-app15-bg$b 0 15 $b -- taskset -c 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29 $silo_path/out-perf.masstree/benchmarks/dbtest --verbose --bench ycsb --num-threads 15 --scale-factor 400005 --ops-per-worker=100000000 --slow-exit --numa-memory 60G;
+    DRAMSIZE=20761804800 $scripts_path/linux.sh tpp-silo-ycsb-app15-bg$b 0 15 $b -- taskset -c 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29 $silo_path/out-perf.masstree/benchmarks/dbtest --verbose --bench ycsb --num-threads 15 --scale-factor 400005 --ops-per-worker=100000000 --slow-exit;
     end_time=$(date +%s)
     elapsed_time=$(($end_time - $start_time))
     echo "Took $elapsed_time seconds" >> $stats_path/tpp-silo-ycsb-app15-bg$b.time.txt
@@ -16,7 +16,7 @@ done;
 
 for b in 0 5 10 15; do
     start_time=$(date +%s)
-    DRAMSIZE=20761804800 $scripts_path/linux-colloid.sh tpp-colloid-silo-ycsb-app15-bg$b 0 15 $b -- taskset -c 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29 $silo_path/out-perf.masstree/benchmarks/dbtest --verbose --bench ycsb --num-threads 15 --scale-factor 400005 --ops-per-worker=100000000 --slow-exit --numa-memory 60G;
+    DRAMSIZE=20761804800 $scripts_path/linux-colloid.sh tpp-colloid-silo-ycsb-app15-bg$b 0 15 $b -- taskset -c 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29 $silo_path/out-perf.masstree/benchmarks/dbtest --verbose --bench ycsb --num-threads 15 --scale-factor 400005 --ops-per-worker=100000000 --slow-exit;
     end_time=$(date +%s)
     elapsed_time=$(($end_time - $start_time))
     echo "Took $elapsed_time seconds" >> $stats_path/tpp-colloid-silo-ycsb-app15-bg$b.time.txt
