078391cfae94b95b8f296f1231367010f9d7a538
Midhul <midhul.v@gmail.com>
2024-09-10 18:50:09 +0000

    Update scripts

--- sosp24-artifact/memtis/run_fig8b.sh
+++ sosp24-artifact/memtis/run_fig8b.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
-# Silo
+# silo
 
 scripts_path="${BASH_SOURCE%/*}/../../scripts"
 
 source $scripts_path/config.sh
 
+prefix="memtis"
+ns_arg=""
+if [ -n "${MEMTIS_NS}" ]; then
+    echo "Disabling page size determination";
+    prefix="memtis-ns"
+    ns_arg="-NS"
+fi
+
 for b in 0 5 10 15; do
     start_time=$(date +%s)
-    DRAMSIZE=20761804800 HEMEM_BASELINE="y" $scripts_path/hemem.sh hemem-silo-ycsb-app15-bg$b 0 15 $b -- taskset -c 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29 $silo_path/out-perf.masstree/benchmarks/dbtest --verbose --bench ycsb --num-threads 15 --scale-factor 400005 --ops-per-worker=100000000 --slow-exit --numa-memory 60G;
+    $scripts_path/memtis.sh $prefix-silo-ycsb-app15-bg$b 15 $b -- $memtis_path/memtis-userspace/scripts/run_bench.sh -B silo -R 1:2 --cxl $ns_arg -V $prefix-silo-ycsb-app15-bg$b
     end_time=$(date +%s)
     elapsed_time=$(($end_time - $start_time))
-    echo "Took $elapsed_time seconds" >> $stats_path/hemem-silo-ycsb-app15-bg$b.time.txt
+    echo "Took $elapsed_time seconds" >> $stats_path/memtis-silo-ycsb-app15-bg$b.time.txt
 done;
 
 for b in 0 5 10 15; do
     start_time=$(date +%s)
-    DRAMSIZE=20761804800 $scripts_path/hemem.sh hemem-colloid-silo-ycsb-app15-bg$b 0 15 $b -- taskset -c 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29 $silo_path/out-perf.masstree/benchmarks/dbtest --verbose --bench ycsb --num-threads 15 --scale-factor 400005 --ops-per-worker=100000000 --slow-exit --numa-memory 60G;
+    $scripts_path/memtis-colloid.sh $prefix-colloid-silo-ycsb-app15-bg$b 15 $b -- $memtis_path/memtis-userspace/scripts/run_bench.sh -B silo-pr -R 1:2 --cxl $ns_arg -V $prefix-colloid-silo-ycsb-app15-bg$b 
     end_time=$(date +%s)
     elapsed_time=$(($end_time - $start_time))
-    echo "Took $elapsed_time seconds" >> $stats_path/hemem-colloid-silo-ycsb-app15-bg$b.time.txt
+    echo "Took $elapsed_time seconds" >> $stats_path/memtis-colloid-silo-ycsb-app15-bg$b.time.txt
 done;
+
+
+
 
 
