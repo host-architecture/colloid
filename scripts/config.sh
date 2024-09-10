53fa53d7e8a880a8f9eebe50996905d57af0a110
Midhul <midhul.v@gmail.com>
2024-09-10 17:35:18 +0000

    Update GAPBS scripts

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,8 +1,8 @@
 
 # Configs
 
-stats_path=/home/sosp24ae/colloid-eval
-gapbs_path=/home/sosp24ae/gapbs
-silo_path=/home/sosp24ae/silo
-cachelib_path=/home/sosp24ae/CacheLib
+stats_path=/home/midhul/membw-eval
+gapbs_path=/home/midhul/gapbs
+silo_path=/home/midhul/silo
+cachelib_path=/home/midhul/CacheLib
 memtis_path=/home/midhul/memtis
