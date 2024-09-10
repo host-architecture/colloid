f8ce07a0e69001142ba3f7c161ad4689191b5922
Midhul <midhul.v@gmail.com>
2024-09-10 06:59:29 +0000

    Update scripts

--- sosp24-artifact/memtis/run_fig8a.sh
+++ sosp24-artifact/memtis/run_fig8a.sh
@@ -6,20 +6,28 @@ scripts_path="${BASH_SOURCE%/*}/../../scripts"
 
 source $scripts_path/config.sh
 
+prefix="memtis"
+ns_arg=""
+if [ -n "${MEMTIS_NS}" ]; then
+    echo "Disabling page size determination";
+    prefix="memtis-ns"
+    ns_arg="-NS"
+fi
+
 for b in 0 5 10 15; do
     start_time=$(date +%s)
-    DRAMSIZE=4404019200 MMAP_PRE_POPULATE=1 MMAP_FILTER_SIZE=1024 HEMEM_BASELINE="y" $scripts_path/hemem.sh hemem-gapbs-twitter-1to2-app15-bg$b 0 15 $b -- taskset -c 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29 $gapbs_path/pr -f $gapbs_path/benchmark/graphs/twitter.sg -i1000 -t1e-4 -n20;
+    $scripts_path/memtis.sh $prefix-gapbs-twitter-1to2-app15-bg$b 15 $b -- $memtis_path/memtis-userspace/scripts/run_bench.sh -B gapbs-pr -R 1:2 --cxl $ns_arg -V $prefix-gapbs-twitter-1to2-app15-bg$b
     end_time=$(date +%s)
     elapsed_time=$(($end_time - $start_time))
-    echo "Took $elapsed_time seconds" >> $stats_path/hemem-gapbs-twitter-1to2-app15-bg$b.time.txt
+    echo "Took $elapsed_time seconds" >> $stats_path/memtis-gapbs-twitter-1to2-app15-bg$b.time.txt
 done;
 
 for b in 0 5 10 15; do
     start_time=$(date +%s)
-    DRAMSIZE=4404019200 MMAP_PRE_POPULATE=1 MMAP_FILTER_SIZE=1024 $scripts_path/hemem.sh hemem-colloid-gapbs-twitter-1to2-app15-bg$b 0 15 $b -- taskset -c 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29 $gapbs_path/pr -f $gapbs_path/benchmark/graphs/twitter.sg -i1000 -t1e-4 -n20; 
+    $scripts_path/memtis-colloid.sh $prefix-colloid-gapbs-twitter-1to2-app15-bg$b 15 $b -- $memtis_path/memtis-userspace/scripts/run_bench.sh -B gapbs-pr -R 1:2 --cxl $ns_arg -V $prefix-colloid-gapbs-twitter-1to2-app15-bg$b 
     end_time=$(date +%s)
     elapsed_time=$(($end_time - $start_time))
-    echo "Took $elapsed_time seconds" >> $stats_path/hemem-colloid-gapbs-twitter-1to2-app15-bg$b.time.txt
+    echo "Took $elapsed_time seconds" >> $stats_path/memtis-colloid-gapbs-twitter-1to2-app15-bg$b.time.txt
 done;
 
 
