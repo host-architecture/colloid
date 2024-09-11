b902f5698d4d8725acc049f6bbdb558637e5eeee
Midhul <midhul.v@gmail.com>
2024-09-11 21:20:38 +0000

    update scripts

--- sosp24-artifact/memtis/run_fig6.sh
+++ sosp24-artifact/memtis/run_fig6.sh
@@ -8,28 +8,37 @@ trap resetme EXIT
 
 scripts_path="${BASH_SOURCE%/*}/../../scripts"
 gups_path="${BASH_SOURCE%/*}/../../apps/gups"
-duration=100
+duration=120
 i=15 # no. of app cores
-bg_cores=$(($1*5)) #background traffic cores (0x => 0, 1x => 5, 2x => 10, 3x => 15)
+# bg_cores=$(($1*5)) #background traffic cores (0x => 0, 1x => 5, 2x => 10, 3x => 15)
+source $scripts_path/config.sh
 
-# HeMem
-echo "Running HeMem"
+prefix="memtis"
+ns_arg=""
+if [ -n "${MEMTIS_NS}" ]; then
+    echo "Disabling page size determination";
+    prefix="memtis-ns"
+    ns_arg="-NS"
+fi
+
+# MEMTIS
+echo "Running MEMTIS"
 for u in 0x70e 0x50a 0x408; do 
     wrmsr -p 0 0x620 $u; 
     echo "uncore freq register: $(sudo rdmsr -p 0 0x620)"; 
-    for b in $bg_cores; do 
-        MMAP_PRE_POPULATE=1 HEMEM_BASELINE="y" $scripts_path/hemem.sh hemem-unc$u-gups64-rw-app$i-bg$b $duration $i $b -- $gups_path/gups64-rw $i; 
+    for b in 0 5 10 15; do
+        MEMTIS_GUPS_CORES=$i MEMTIS_GUPS_DURATION=$duration $scripts_path/memtis.sh $prefix-unc$u-gups64-rw-app$i-bg$b $i $b -- $memtis_path/memtis-userspace/scripts/run_bench.sh -B gups -R gups64-rw --cxl $ns_arg -V $prefix-unc$u-gups64-rw-app$i-bg$b
     done; 
 done; 
 wrmsr -p 0 0x620 0x818;
 
 # HeMem+colloid
-echo "Running HeMem+colloid"
+echo "Running MEMTIS+colloid"
 for u in 0x70e 0x50a 0x408; do 
     wrmsr -p 0 0x620 $u; 
     echo "uncore freq register: $(sudo rdmsr -p 0 0x620)"; 
-    for b in $bg_cores; do 
-        MMAP_PRE_POPULATE=1 $scripts_path/hemem.sh hemem-colloid-unc$u-gups64-rw-app$i-bg$b $duration $i $b -- $gups_path/gups64-rw $i; 
+    for b in 0 5 10 15; do 
+        MEMTIS_GUPS_CORES=$i MEMTIS_GUPS_DURATION=$duration $scripts_path/memtis-colloid.sh $prefix-colloid-unc$u-gups64-rw-app$i-bg$b $i $b -- $memtis_path/memtis-userspace/scripts/run_bench.sh -B gups -R gups64-rw --cxl $ns_arg -V $prefix-colloid-unc$u-gups64-rw-app$i-bg$b 
     done; 
 done; 
 wrmsr -p 0 0x620 0x818;
