acdc217de22ed674f2e3539594b84a59dc2d0f2b
Midhul <midhul.v@gmail.com>
2024-09-11 13:52:52 +0000

    update scripts

--- sosp24-artifact/memtis/collect_fig8c.sh
+++ sosp24-artifact/memtis/collect_fig8c.sh
@@ -5,11 +5,13 @@ i=15 # no. of app cores
 
 source $scripts_path/config.sh
 
-printf "%-15s %-15s %-15s %-15s\n" "Intensity" "HeMem" "HeMem+colloid" "Perf. Improvement (%)"
+results_path=$memtis_path/memtis-userspace/results/cachelib
+
+printf "%-15s %-15s %-15s %-15s\n" "Intensity" "memtis" "memtis+colloid" "Perf. Improvement (%)"
 
 for b in 0 5 10 15; do
-    hemem_output=$(cat $stats_path/hemem-cachelib-hememkv-app15-bg$b.app.txt | grep -i "get       :" | awk '{print $3}' | tr -d '/,s')
-    hemem_colloid_output=$(cat $stats_path/hemem-colloid-cachelib-hememkv-app15-bg$b.app.txt | grep -i "get       :" | awk '{print $3}' | tr -d '/,s')
-    impr=$(awk -v num="$hemem_colloid_output" -v den="$hemem_output" 'BEGIN {print (num/den - 1.0)*100.0;}')
-    printf "%-15s %-15s %-15s %-15s\n" "$(($b/5))x" "$hemem_output" "$hemem_colloid_output" "$impr"
+    memtis_output=$(cat $results_path/memtis-cachelib-hememkv-app15-bg$b/output.log | grep -i "get       :" | awk '{print $3}' | tr -d '/,s')
+    memtis_colloid_output=$(cat $results_path/memtis-colloid-cachelib-hememkv-app15-bg$b/output.log | grep -i "get       :" | awk '{print $3}' | tr -d '/,s')
+    impr=$(awk -v num="$memtis_colloid_output" -v den="$memtis_output" 'BEGIN {print (num/den - 1.0)*100.0;}')
+    printf "%-15s %-15s %-15s %-15s\n" "$(($b/5))x" "$memtis_output" "$memtis_colloid_output" "$impr"
 done
